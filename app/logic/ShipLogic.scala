1bb14dd808e570303e3ad91336e0489b57357c38
adamnfish <adamnfish@gmail.com>
2016-04-25 23:49:27 +0100

    Add special case for Ghost
    Ensures the ghost attack shuttle is added whenever the ghost is
    present.

--- app/logic/ShipLogic.scala
+++ app/logic/ShipLogic.scala
@@ -1,13 +1,20 @@
 package logic
 
+import data.Ships
 import models.Ship
 
 object ShipLogic {
   def shipcodesFromQueryString(rawQueryString: String): List[String] = {
-    for {
+    val ships = for {
       entry <- rawQueryString.split("&").toList
       code <- entry.split("=").headOption
     } yield code
+
+    if (ships.contains(Ships.ghost.code) && !ships.contains(Ships.ghostAttackShuttle.code)) {
+      ships :+ Ships.ghostAttackShuttle.code
+    } else {
+      ships
+    }
   }
 
   def lookupShips(allShips: List[Ship], codes: List[String]): List[Ship] = {
